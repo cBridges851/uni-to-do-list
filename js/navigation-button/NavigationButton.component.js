fb5f28ca5d104babaa709f91ee4bacc217dd4a2d
Christa Bridges <christa@pebblepad.co.uk>
2021-05-08 15:06:01 +0100

    browse-todos The view button now has functionality. When the user clicks on it, they see the browse todos view.

--- js/navigation-button/NavigationButton.component.js
+++ js/navigation-button/NavigationButton.component.js
@@ -1,6 +1,15 @@
+class NavigationButtonComponent {
+    navigate() {
+        if (this.section === "View") {
+            window.open("#!/browse-todos", "_self")
+        }
+    }
+}
+
 export const navigationButtonComponentDefinition = {
     bindings: {
         section: "="
     },
     templateUrl: "./js/navigation-button/navigation-button.html",
+    controller: NavigationButtonComponent
 };
