c804f96e6e81db5874cda8962da46325d64031a5
Christa Bridges <christa@pebblepad.co.uk>
2021-05-08 12:42:04 +0100

    navigation-bar Created the navigation component as well as its child, which is simply a button.

--- js/AppRegistration.effect.js
+++ js/AppRegistration.effect.js
@@ -1,1 +1,6 @@
 import { app } from "./App.const.js";
+import { navigationComponentDefinition } from "./navigation/Navigation.component.js";
+import { navigationButtonComponentDefinition } from "./navigation-button/NavigationButton.component.js";
+
+app.component("navigationButton", navigationButtonComponentDefinition);
+app.component("navigation", navigationComponentDefinition);
