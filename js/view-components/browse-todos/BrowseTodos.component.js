222f6e0ed4da2d072b5dd7466935f7635ed1b247
Christa Bridges <christa@pebblepad.co.uk>
2021-05-08 14:04:10 +0100

    browse-todos The items that are to do are displayed in the browse to do views. List items will now be split into their own components.

--- js/view-components/browse-todos/BrowseTodos.component.js
+++ js/view-components/browse-todos/BrowseTodos.component.js
@@ -1,3 +1,21 @@
+class BrowseTodosComponent {
+    constructor() {
+        this.todos = [
+            {
+                "task": "Example task 1",
+                "module": "Introduction to programming",
+                "completed": false
+            },
+            {
+                "task": "Example task 2",
+                "module": "Introduction to programming",
+                "completed": false
+            }
+        ];
+    }
+} 
+
 export const browseTodosComponentDefintion = {
-    templateUrl: "./js/view-components/browse-todos/browse-todos.html"
+    templateUrl: "./js/view-components/browse-todos/browse-todos.html",
+    controller: BrowseTodosComponent
 }
